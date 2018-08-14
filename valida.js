8a694483c6d51c660e71e6490f694c06289158ac
Bianca <biaamesquitaa@gmail.com>
2018-08-14 01:29:21 +0000

    atualizando html e incluindo js e php

---
+++ valida.js
@@ -0,0 +1,11 @@
+<script>
+function validarSenha(form){ 
+	senha = document.formulario.senha.value
+	senhaRepetida = document.formulario.repetir_senha.value
+	if (senha != senhaRepetida){
+		alert("Repita a senha corretamente");
+		document.formulario.repetir_senha.focus();	
+		return false;
+	}
+}
+</script>
